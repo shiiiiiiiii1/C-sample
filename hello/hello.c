bfdc44f21d8eefc6779a38a2835f43a8d7309035
shoichi <shou1_822@yahoo.co.jp>
2016-05-31 23:00:33 +0900

    deta type

--- hello/hello.c
+++ hello/hello.c
@@ -1,8 +1,28 @@
 #include <stdio.h>	//決まり文句　標準入出力　便利な機能が詰まったもの
 
-/* コメントアウト */
+/*
+第一回
+コメントアウト
+*/
+
+/*
+第二回
+データ型の主な種類
+int:整数 %d
+float:実数 %f
+char:1文字 %c
 
 int main(void) {
-	printf("hello world\n");
+	int x = 10;
+	float y = 5.2;
+	char c = 'A';
+
+	printf("x = %d, y = %f, c = %c\n", x, y, c);
 	return 0;
 }
+*/
+
+/*
+第三回
+
+*/
